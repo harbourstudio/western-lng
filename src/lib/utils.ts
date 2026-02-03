8e1bd1ac7413f137086fa530ea4006bd4150cf14
juls-codes <biayj@protonmail.com>
2026-02-02 18:05:02 -0800

    perf: move cleanString to utility file to make it global

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,12 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+// Function to remove zero-width and invisible Unicode characters
+export function cleanString(str: string | undefined): string {
+  if (!str) return '';
+  return str
+    .replace(/[\u200B-\u200D\uFEFF\u202A-\u202E]/g, '')
+    .replace(/[\u061C\u180E\u2066-\u2069]/g, '')
+    .trim();
+}
